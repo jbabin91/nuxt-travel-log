fec198ef06ff0e9f20266a57952d51856eb4536a
Jace Babin <jbabin91@gmail.com>
2025-05-02 15:51:47 -0500

    chore: update lint-staged configuration for file matching
    - Changed file matching pattern in .lintstagedrc.js from "*.(js|jsx|ts|tsx)" to "*.{js,jsx,ts,tsx}" for improved compatibility
    - Ensures type checking is applied correctly to all specified file types

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@
 /** @type {import("lint-staged").Configuration} */
 export default {
   // Type check TypeScript files
-  "*.(js|jsx|ts|tsx)": () => "pnpm typecheck",
+  "*.{js,jsx,ts,tsx}": () => "pnpm typecheck",
   // Lint/Format files
   "*": () => "pnpm lint",
 };
