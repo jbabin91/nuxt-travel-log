3fe57e40a3b0847d278c2bc0699dc5273ea16474
Jace Babin <jbabin91@gmail.com>
2025-05-02 18:32:48 -0500

    feat(ui,config,deps): ✨ restructure application with new components and styles
    - Replaced the old app.vue with a new structure that utilizes NuxtLayout and NuxtPage for better organization
    - Created a new main.css file to import Tailwind CSS and configure DaisyUI themes
    - Added a navigation bar component (nav-bar.vue) for improved user navigation
    - Introduced a default layout (default.vue) to wrap pages with a consistent structure
    - Developed an index page (index.vue) with a hero section to enhance the landing experience
    - Updated nuxt.config.ts to reflect the new src directory structure
    - Ensured all new components and styles are integrated into the application for a cohesive look and feel

---
+++ src/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="navbar bg-primary text-primary-content">
+    <nav class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        daisyUI
+      </NuxtLink>
+    </nav>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With GitHub
+        <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </header>
+</template>
