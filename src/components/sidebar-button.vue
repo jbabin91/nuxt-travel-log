74661ae6eb16057a417d35c7eb0b8e631d00a97d
Jace Babin <jbabin91@gmail.com>
2025-05-06 11:26:29 -0700

    feat(ui): add sidebar button component and enhance dashboard layout (#64)
    - Introduced a new SidebarButton component for navigation with
    customizable labels and icons
    - Updated the dashboard layout to include a collapsible sidebar,
    improving user experience
    - Implemented sidebar toggle functionality to store state in local
    storage
    - Enhanced the main content area to adapt based on sidebar visibility

---
+++ src/components/sidebar-button.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const { label, icon, href, showLabel } = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="tooltip-right" :data-tip="showLabel ? undefined : label" :class="{ tooltip: !showLabel }">
+    <NuxtLink :to="href" :class="{ 'bg-base-200 font-bold': route.path === href, 'justify-center': !showLabel, 'justify-start': showLabel }" class="flex flex-nowrap gap-2 p-2 hover:bg-base-300 hover:cursor-pointer">
+      <Icon :name="icon" size="24" />
+      <Transition name="grow">
+        <span v-if="showLabel">
+          {{ label }}
+        </span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1s;
+}
+.grow-leave-active {
+  animation: grow 0.1s reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
